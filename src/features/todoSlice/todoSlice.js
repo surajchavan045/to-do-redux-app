e4646a52c966c4d2feafa96769a84c1b26cbad39
unknown <sc465182@gmail.com>
2025-04-19 14:28:38 +0530

    added important task page

--- src/features/todoSlice/todoSlice.js
+++ src/features/todoSlice/todoSlice.js
@@ -1,21 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getTodos = createAsyncThunk("todo/getTodos", async () => {
+  const res = await fetch("https://dummyjson.com/todos"); // Promise --> resolve/reject
+  const data = await res.json(); // res --> json to js object
+  //
+
+  // const data = await axios.get("https://dummyjson.com/todos");
+
+  return data.todos; // todos --> []
+});
 
 const todoSlice = createSlice({
   name: "todo",
-  initialState: { todos: [] },
+  initialState: { todos: [], loading: false, error: false },
   reducers: {
     addTodo: (state, action) => {
       // action.payload contains value passed inside addTodo function while dispatching
+      console.log(action);
       state.todos.push(action.payload);
     },
     removeTodo: (state, action) => {
       // state.todos
+
       const index = action.payload;
 
       state.todos.splice(index, 1);
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTodos.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getTodos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload;
+      })
+      .addCase(getTodos.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      });
+  },
 });
 
 export const todoReducer = todoSlice.reducer;
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions;
--- src/pages/ImportatntTasks.jsx
+++ src/pages/ImportatntTasks.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTodos } from "../features/todoSlice/todoSlice";
 
 const ImportatntTasks = () => {
-  return <div>ImportatntTasks</div>;
+  // throw new Error("slkdnsldkf");
+
+  const todoState = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
+
+  if (todoState.error) {
+    return (
+      <div className="ml-[20vw] bg-neutral-800 p-4 min-h-screen text-red-600">
+        <h1>Something went wrong ....</h1>
+      </div>
+    );
+  }
+
+  if (todoState.loading) {
+    return (
+      <div className="ml-[20vw] bg-neutral-800 p-4 min-h-screen text-neutral-200">
+        <h1>Loading ....</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="ml-[20vw] bg-neutral-800 p-4 min-h-screen text-neutral-200">
+      {todoState.todos.map((todo) => {
+        return <li key={todo.id}>{todo.todo}</li>;
+      })}
+    </div>
+  );
 };
 
 export default ImportatntTasks;
